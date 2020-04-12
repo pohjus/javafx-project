f9e1fab6d9d491cd29a5606589c92f289b56565b
pohjus <jussi.pohjolainen@tuni.fi>
2020-04-12 11:41:43 +0300

    some change

--- src/main/java/com/company/App.java
+++ src/main/java/com/company/App.java
@@ -6,10 +6,10 @@ import javafx.stage.Stage;
 public class App extends Application {
     @Override
     public void start(Stage stage) {
-        stage.setTitle("JavaFX moi!");
+        stage.setTitle("Jussi Pohjolainen!");
         stage.show();
     }
-    public static void main(String args[]) {
+    public static void main(String... args) {
         launch(args);
     }
 }
